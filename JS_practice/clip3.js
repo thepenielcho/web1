4b61da8df1a32377f8a4959e7167b88b53335614
Peniel Cho <pensurf@naver.com>
2021-04-30 23:56:06 +0900

    // function lines

--- JS_practice/clip3.js
+++ JS_practice/clip3.js
@@ -294,14 +294,23 @@
 //     console.log(test());
 // }
 
-const hello1 = () => {
-    console.log('hello1');
-};
-
-const hello2 = name => {
-    console.log('hello2', name);
-};
-
-const hello3 = (name, age) => {
-    console.log('hello3', name, age);
-};
+// const hello1 = () => {
+//     console.log('hello1');
+// };
+//
+// const hello2 = name => {
+//     console.log('hello2', name);
+// };
+// // 매개변수가 하나일 때 괄호 생략 가능
+//
+// const hello3 = (name, age) => {
+//     console.log('hello3', name, age);
+// };
+//
+// const hello4 = name => {
+//     return `hello4 ${name}`;
+// }
+//
+// const hello5 = name => `hello5 ${name}`;
+//
+// // arrow 함수의 경우 return 키워드와 {} 생략 가능!
