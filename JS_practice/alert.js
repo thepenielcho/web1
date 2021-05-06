7f27417d30b82c6c440145136059a4c9f90fe765
Peniel Cho <pensurf@naver.com>
2021-05-06 23:50:51 +0900

    js info study

---
+++ JS_practice/alert.js
@@ -0,0 +1,15 @@
+// alert('Hello World!');
+// console.log('this you?');
+
+// let admin='Peniel';
+// let name='John';
+// admin=name;
+// alert(admin);
+
+// let ourPlanetName='Earth';
+// let currentUserName;
+
+let name = 'Ilya';
+alert(`hello ${1}`); //hello 1
+alert(`hello ${"name"}`); // hello name  // hello "name"이 아님!
+alert(`hello ${name}`); //hello Ilya
