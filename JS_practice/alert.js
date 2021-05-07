631d3242fabb703330961d105574a2c0267a695d
Peniel Cho <pensurf@naver.com>
2021-05-07 23:46:47 +0900

    alert etc

--- JS_practice/alert.js
+++ JS_practice/alert.js
@@ -9,7 +9,16 @@
 // let ourPlanetName='Earth';
 // let currentUserName;
 
-let name = 'Ilya';
-alert(`hello ${1}`); //hello 1
-alert(`hello ${"name"}`); // hello name  // hello "name"이 아님!
-alert(`hello ${name}`); //hello Ilya
+// let name = 'Ilya';
+// alert(`hello ${1}`); //hello 1
+// alert(`hello ${"name"}`); // hello name  // hello "name"이 아님!
+// alert(`hello ${name}`); //hello Ilya
+
+// let age = prompt('나이를 입력해주세요', 100);
+// alert(`당신의 나이는 ${age}살 입니다.`)
+
+// let isBoss = confirm("당신이 주인인가요?");
+// alert(isBoss);
+
+let userName = prompt("당신의 이름은 무엇인가요?", '홍길동');
+alert(userName);
